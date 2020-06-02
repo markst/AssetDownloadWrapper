958ddecec22d4f0e9149269a2bd40067613ec97e
Mark Turner <git@markst.co.uk>
2020-06-02 14:01:12 +1000

    Adding cancel button to sample project.

--- AssetDownloadWrapper-Sample/AssetDownloadWrapper-Sample/ViewController.swift
+++ AssetDownloadWrapper-Sample/AssetDownloadWrapper-Sample/ViewController.swift
@@ -69,11 +69,26 @@ class ViewController: UIViewController {
         vc.player = player
         present(vc, animated: true, completion: nil)
     }
-    
+
+    @IBAction private func cancelButtonTapped(_ sender: Any) {
+        AssetDownloadManager.shared
+            .cancelDownload(
+                for: .init(
+                    urlAsset: .init(url: Self.url),
+                    assetTitle: Self.name
+                )
+        )
+    }
+
+    // MARK: -
     private var player = AVPlayer()
+
+    // MARK: - Outlets
     @IBOutlet private weak var progressLabel: UILabel!
     @IBOutlet private weak var startButton: UIButton!
+    @IBOutlet private weak var cancelButton: UIButton!
     @IBOutlet private weak var progressView: UIProgressView!
+
     private static let name = "bipbop_4x3_variant"
     private static let url = URL(string: "https://mnmedias.api.telequebec.tv/m3u8/29880.m3u8")!
 }
